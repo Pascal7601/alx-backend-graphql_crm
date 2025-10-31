04e96f03c063200ab4cf7ea3b6adf7d6b5437c63
Pascal7601 <levispascal63@gmail.com>
2025-10-31 10:48:32 +0300

    feat: set up the graphql UI and wrote a simple query

---
+++ alx_backend_graphql_crm/schema.py
@@ -0,0 +1,8 @@
+from graphene_django import DjangoObjectType
+import graphene
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello, GraphQL!")
+
+
+schema = graphene.Schema(query=Query)
